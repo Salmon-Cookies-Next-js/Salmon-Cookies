775566b66fcdab60f492f00d0e3da3fde663f68b
Tiara Brown <90294860+tiarabcodes1@users.noreply.github.com>
2022-05-20 13:24:10 -0400

    Noted state management may be viable with useEffect

--- pages/sales.js
+++ pages/sales.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+//Can useEffect help data state rendering/updating for form?
 export default function Sales() {
 const storesArray = [];
 let hours = ['6 a.m', '7 a.m', '8 a.m', '9 a.m.','10 a.m','11 a.m','12 p.m','1 p.m','2 p.m','3 p.m','4 p.m','5 p.m','6 p.m','7 p.m','Daily location Total'];
